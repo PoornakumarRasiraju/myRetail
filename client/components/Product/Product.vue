c79591c0cc11d8397804619a5b3e2c8bbf4362b8
Poornakumar <prasiraju@axxess.com>
2018-03-25 23:35:58 -0500

    Image slider:

--- client/components/Product/Product.vue
+++ client/components/Product/Product.vue
@@ -9,19 +9,23 @@
         </div>
         <div class="product-images">
             <i @click="slideLeft" class="fas fa-angle-left"></i>
-            <ul class="product-image-slider">
-                <li v-for="(productImage, index) in AlternateImages" :key="productImage.id">
-                    <button :class="{active : isActive}" @click="viewImage($event, index)">
-                        <img width="50" height="50" :src="productImage.image">
-                    </button>
-                </li>
-            </ul>
+            <div>
+                <ul class="product-image-slider">
+                    <li v-for="(productImage, index) in AlternateImages" :key="productImage.id">
+                        <button @click="viewImage($event, index)">
+                            <img width="50" height="50" :src="productImage.image">
+                        </button>
+                    </li>
+                </ul>
+            </div>
             <i @click="slideRight" class="fas fa-angle-right"></i>
         </div>
 	</section>
 </template>
 
 <script>
+import $ from 'jQuery';
+
 export default {
     name: 'Product',
     components: { 
@@ -48,28 +52,39 @@ export default {
     methods: {
         viewImage($event, index) {
             let selectedImage = $event.currentTarget.querySelectorAll('img')[0].src;
-
             this.imageCounter = index+1;
             this.primaryImage = selectedImage;
+
+            $('.product-image-slider li').removeClass('selected');
+            $('.product-image-slider li:eq('+index+')').addClass('selected');
         },
         slideLeft($event) {
+            $('.product-image-slider li').removeClass('selected');
+
             if(this.imageCounter > 1 &&  this.imageCounter <= this.AlternateImages.length) {
                 console.log(this.imageCounter);
                 this.imageCounter--;
+                let imageSlide = this.imageCounter-1;
+                $('.product-image-slider li:eq('+imageSlide+')').addClass('selected');
                 this.primaryImage = this.$store.state.CatalogEntryView[0].Images[0].AlternateImages[this.imageCounter-1].image;
             } else {
                 this.primaryImage = this.$store.state.CatalogEntryView[0].Images[0].AlternateImages[this.AlternateImages.length-1].image;
                 this.imageCounter = this.AlternateImages.length - 1;
+                $('.product-image-slider li:eq('+this.imageCounter+')').addClass('selected');
             }
         },
         slideRight($event) {
+            $('.product-image-slider li').removeClass('selected');
+
             if(this.imageCounter < this.AlternateImages.length) {
                 console.log(this.imageCounter);
+                $('.product-image-slider li:eq('+this.imageCounter+')').addClass('selected');
                 this.primaryImage = this.$store.state.CatalogEntryView[0].Images[0].AlternateImages[this.imageCounter].image;
                 this.imageCounter++;
             } else {
                 this.primaryImage = this.$store.state.CatalogEntryView[0].Images[0].AlternateImages[0].image;
                 this.imageCounter = 1;
+                $('.product-image-slider li:eq('+0+')').addClass('selected');
             }
         }
     }
@@ -137,15 +152,23 @@ section {
     display: flex;
     list-style: none;
     padding-left: 10px;
+    padding-right: 10px;
     margin-top: 26px;
     margin-bottom: 25px;
-    width: 33%;
+    width: 220px;
     position: relative;
     overflow: hidden;
 
     li {
+        border: 1px solid transparent;
         cursor: pointer;
+        position: relative;
         margin-right: 10px;
+    }
+
+    .selected {
+        border: 1px solid #D6D6D6;
+        border-radius: 4px;
     }
 
     button {
